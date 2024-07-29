b88dbbe2d143aba868807c169c0601b43908f377
Shrujan S Shetty <142001419+shrujansshetty@users.noreply.github.com>
2024-07-29 23:51:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu(x) {
+    x.classList.toggle("change");
+    const sideMenu = document.getElementById("side-menu");
+    if (sideMenu.style.width === "250px") {
+        sideMenu.style.width = "0";
+    } else {
+        sideMenu.style.width = "250px";
+    }
+}
